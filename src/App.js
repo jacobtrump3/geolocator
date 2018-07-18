8e0d648f72d5d4f28c4b59b7449920c67595035b
Jacob Trump <jacob.trump@ad.infosys.com>
2018-07-18 12:56:39 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import Geolocator2 from './geolocator2';
-import MarkerMap from './MarkerMap';
 class App extends Component {
     render() {
       return (
         <div className="App">
-            <MarkerMap/>
-            <div></div>
             <Geolocator2/>
         </div>
       );
